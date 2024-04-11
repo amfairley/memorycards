69257edb9ec4cc125c665ed649e731a2b73bad2b
amfairley <adamfairley1990@gmail.com>
2024-04-11 10:34:07 +0100

    function: add functionality to append card images to game area

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,8 +14,8 @@ const cards = [
     'qc.png',
     'kc.png'
 ];
-let shuffledCards;
 let sequenceLength;
+let cardsToPlay;
 
 // This shuffling algorithm was made with help from stack overflow and Bro Code on YouTube, see README.md for links
 /**
@@ -34,3 +34,38 @@ function shuffle(array) {
       }
     return array;
     }
+
+/**
+ *  Cycles through n times, making divs housing the card images and appending them to the game area
+*/ 
+function makeDivs(n) {
+    for (let i = 0; i < n; i++) {
+        var newDiv = document.createElement("div");
+        // Assign CSS class
+        $(newDiv).addClass("col-6 col-sm-4 col-lg-2 card-div");
+        // Assign an id of box + whatever number box it is-----------------NEEDED?
+        $(newDiv).attr("id","cardimage"+i);
+        // Add image to the div
+        $(newDiv).html("<img src='assets/images/cards/"+cardsToPlay[i]+"' class = 'card-image'>")
+        // Apend it
+        $("#game-area").append(newDiv);
+    }
+}
+// Button event listeners
+
+// Apply length button chooses how many cards to show, shuffles the cards, displays the play button
+$("#sequence-length").click(function() {
+    // save the number as length
+    sequenceLength = $("#quantity").val();
+    // shuffle the cards
+    shuffle(cards);
+});
+
+// Play button 
+$("#play").click(function() {
+    // Selects a portion of the shuffled cards to play
+    cardsToPlay = cards.slice(0, sequenceLength);
+    // Make separate divs
+    makeDivs(sequenceLength);
+})
+
