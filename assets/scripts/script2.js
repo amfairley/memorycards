1f5e2a16e33e6bb87fe4896346ccdba7bf78782f
amfairley <adamfairley1990@gmail.com>
2024-04-11 19:47:11 +0100

    function: track the user score and display at the end

--- assets/scripts/script2.js
+++ assets/scripts/script2.js
@@ -15,6 +15,10 @@ const cardObject2 = {
     'kc': "King of Clubs"    
 };
 
+let sequenceLength2 = $(".submit-guess").length;
+
+let correctAnswerAmount;
+
 /**
  * Returns the key of an object assigned to a specific value
  */
@@ -25,28 +29,35 @@ function getKeyByValue(object, value) {
 
 
 
+
 // Event listener for class submit-guess
 $(".submit-guess").click(function() {
     // Takes the sibling elements that are select elements in an array
     let siblings = $(this).siblings();
+
     // takes value 1 + value 2 = submitted answer
     let submittedAnswerInitial = siblings[0].value +siblings[1].value;
     console.log(submittedAnswerInitial);
+
     // Get the key from this value in the cardObject2
     let submittedAnswer = cardObject2[String(submittedAnswerInitial)];
+
     // checks this answer against parent's next sibling
     let pSiblings = $(this).parent().siblings();
+
     // Show the answer column
     pSiblings[1].classList.remove("hidden");
     let correctAnswer = pSiblings[1].innerHTML;
     let results = pSiblings[2];
     console.log(correctAnswer);
+
     // Compare the two values
     if (submittedAnswer === correctAnswer) {
         $(results).html("Y");
     } else {
         $(results).html("N");
     };
+
     // Disable the select and submit fields
     $(this).prop("disabled", true);
     $(siblings[0]).prop("disabled", true);
@@ -55,8 +66,24 @@ $(".submit-guess").click(function() {
     // Add a class to the submit button to track submissions
     $(this).addClass("submitted");
 
+    // Check score
+    // Array of the result cell
+    let resultsArray = $(".result");
+    // Score counter starts at 0
+    let counter = 0;
+    // If an answer is correct, add 1 to counter
+    for (let i = 0; i < sequenceLength2; i++) {
+        if (resultsArray[i]['textContent']==="Y") {
+            counter++;
+        }
+    }
+    console.log(counter);
+    let finalScore = Math.floor(100* counter / sequenceLength2);
+
     // An alert to show when the game is finished
-    if ($(".submitted").length === $(".submit-guess").length) {
-        alert("Game over")
-    } 
+    if ($(".submitted").length === sequenceLength2) {
+        alert("Game over. You scored " + finalScore + "%")
+    };
 });
+
+
