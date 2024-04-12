89238887ebc7816d9672c4b14c93396e22e88685
amfairley <adamfairley1990@gmail.com>
2024-04-12 07:59:15 +0100

    function: remove global variables and access those in the other script file

--- assets/scripts/script2.js
+++ assets/scripts/script2.js
@@ -1,35 +1,3 @@
-// Global variables //
-const cardObject2 = {
-    'ac': "Ace of Clubs",
-    '2c': "Two of Clubs",
-    '3c': "Three of Clubs",
-    '4c': "Four of Clubs",
-    '5c': "Five of Clubs",
-    '6c': "Six of Clubs",
-    '7c': "Seven of Clubs",
-    '8c': "Eight of Clubs",
-    '9c': "Nine of Clubs",
-    '0c': "Ten of Clubs",
-    'jc': "Jack of Clubs",
-    'qc': "Queen of Clubs",
-    'kc': "King of Clubs"    
-};
-
-let sequenceLength2 = $(".submit-guess").length;
-
-let correctAnswerAmount;
-
-/**
- * Returns the key of an object assigned to a specific value
- */
-function getKeyByValue(object, value) {
-    return Object.keys(object).find(key => object[key] === value);
-  }
-
-
-
-
-
 // Event listener for class submit-guess
 $(".submit-guess").click(function() {
     // Takes the sibling elements that are select elements in an array
@@ -39,8 +7,8 @@ $(".submit-guess").click(function() {
     let submittedAnswerInitial = siblings[0].value +siblings[1].value;
     console.log(submittedAnswerInitial);
 
-    // Get the key from this value in the cardObject2
-    let submittedAnswer = cardObject2[String(submittedAnswerInitial)];
+    // Get the key from this value in the cardObject
+    let submittedAnswer = cardObject[String(submittedAnswerInitial)];
 
     // checks this answer against parent's next sibling
     let pSiblings = $(this).parent().siblings();
@@ -72,18 +40,16 @@ $(".submit-guess").click(function() {
     // Score counter starts at 0
     let counter = 0;
     // If an answer is correct, add 1 to counter
-    for (let i = 0; i < sequenceLength2; i++) {
+    for (let i = 0; i < sequenceLength; i++) {
         if (resultsArray[i]['textContent']==="Y") {
             counter++;
         }
     }
     console.log(counter);
-    let finalScore = Math.floor(100* counter / sequenceLength2);
+    let finalScore = Math.floor(100* counter / sequenceLength);
 
     // An alert to show when the game is finished
-    if ($(".submitted").length === sequenceLength2) {
+    if ($(".submitted").length === sequenceLength) {
         alert("Game over. You scored " + finalScore + "%")
     };
 });
-
-
